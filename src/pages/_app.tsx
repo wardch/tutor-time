7ff89b86ea6629e3f74603ff95d3f1bece18c840
Charlie Ward <charlieward18@gmail.com>
2023-04-23 13:37:27 +0100

    added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,14 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
       <Toaster position={"bottom-center"} />
       <Component {...pageProps} />
+      <Analytics />
     </ClerkProvider>
   );
 };
